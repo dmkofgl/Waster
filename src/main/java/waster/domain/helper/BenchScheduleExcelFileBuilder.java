d230db97f2e7306f57ceb8501f52e433499eeb52
Uladzislau.Barkou <dmkofgl@gmail.com>
2019-06-10 15:42:47 +0300

    change output in excel

--- src/main/java/waster/domain/helper/BenchScheduleExcelFileBuilder.java
+++ src/main/java/waster/domain/helper/BenchScheduleExcelFileBuilder.java
@@ -1,18 +1,10 @@
 package waster.domain.helper;
 
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xddf.usermodel.*;
-import org.apache.poi.xddf.usermodel.chart.XDDFChartData;
-import org.apache.poi.xssf.usermodel.XSSFChart;
 import org.apache.poi.xssf.usermodel.XSSFDrawing;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.knowm.xchart.XYChart;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTCatAx;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTTitle;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTValAx;
-import org.openxmlformats.schemas.drawingml.x2006.main.CTTextBody;
-import org.openxmlformats.schemas.drawingml.x2006.main.CTTextParagraph;
 import waster.domain.entity.Bench;
 import waster.domain.entity.calendar.Calendar;
 import waster.domain.entity.calendar.Schedule;
@@ -20,27 +12,27 @@ import waster.domain.entity.calendar.Schedule;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.*;
 
 public class BenchScheduleExcelFileBuilder {
+    private Date startDate;
 
     private XSSFWorkbook workbook;
 
-    public BenchScheduleExcelFileBuilder(XSSFWorkbook workbook) {
+
+    public BenchScheduleExcelFileBuilder(Date startDate, XSSFWorkbook workbook) {
+        this.startDate = startDate;
         this.workbook = workbook;
     }
 
-    public void createCommonSheet(Map<Bench, Calendar> benchCalendarMap) {
-        String sheetName = "Main";
+    public void createCommonSheet(Map<Bench, Calendar> benchCalendarMap, Date startDate) {
+        String sheetName = "Общий";
 
-        Date startDate = new Date();
         XSSFSheet sheet = workbook.createSheet(sheetName);
 
         int counter = 0;
 
-        createRow(sheet, counter++, "Номер оборудования", "Метраж", "Артикул", "Время начала", "Длительность", "Время окончания");
+        createRow(sheet, counter++, "Номер оборудования", "Метраж", "Артикул", "Колорит", "Номер заказа", "Время начала", "Длительность", "Время окончания");
         Set<Bench> benches = benchCalendarMap.keySet();
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
@@ -49,40 +41,63 @@ public class BenchScheduleExcelFileBuilder {
         for (Bench bench : benchArrayList) {
             Calendar calendar = benchCalendarMap.get(bench);
             List<Schedule> schedules = calendar.getSchedules();
-            Boolean isFirst = true;
+            boolean isFirst = true;
+
             for (Schedule schedule : schedules) {
-                Row row = sheet.createRow(counter++);
-                Cell c0 = row.createCell(0);
-                if (isFirst) {
-                    c0.setCellValue(bench.getId());
-                    isFirst = false;
-                }
-                Cell c1 = row.createCell(1);
-                c1.setCellValue(schedule.getOperation().getLength());
-
-                Cell c2 = row.createCell(2);
-                //TODO use real name
-                c2.setCellValue(schedule.getOperation().getSetting().toString());
-
-                Cell c3 = row.createCell(3);
                 Date date = new Date(startDate.getTime() + schedule.getStart());
-                c3.setCellValue(dateFormat.format(date));
-                Instant end = Instant.ofEpochMilli(schedule.getEnd());
-                Instant start = Instant.ofEpochMilli(schedule.getStart());
-                Duration duration = Duration.between(start, end);
-
-                Cell c4 = row.createCell(4);
-                c4.setCellValue(duration.toHours() + ":" + duration.toMinutes());
-
-                Cell c5 = row.createCell(5);
-                c5.setCellValue(dateFormat.format(new Date(date.getTime() + schedule.getEnd() - schedule.getStart())));
+                if (isFirst) {
+                    createRow(sheet, counter,
+                            bench.getId().toString(),
+                            schedule.getOperation().getLength().toString(),
+                            schedule.getOperation().getOrder().getArticle().getName(),
+                            schedule.getOperation().getOrder().getArticle().getColoring(),
+                            schedule.getOperation().getOrder().getId().toString(),
+                            dateFormat.format(date),
+                            durationHourAndMinutes(schedule),
+                            dateFormat.format(new Date(date.getTime() + schedule.getEnd() - schedule.getStart()))
+                    );
+                    isFirst = false;
+                } else {
+                    createRow(sheet, counter
+                            , "",
+                            schedule.getOperation().getLength().toString(),
+                            schedule.getOperation().getOrder().getArticle().getName(),
+                            schedule.getOperation().getOrder().getArticle().getColoring(),
+                            schedule.getOperation().getOrder().getId().toString(),
+                            dateFormat.format(date),
+                            durationHourAndMinutes(schedule),
+                            dateFormat.format(new Date(date.getTime() + schedule.getEnd() - schedule.getStart()))
+                    );
+                }
+                counter++;
             }
         }
+    }
 
+    //TODO to test
+    private String durationHourAndMinutes(Schedule schedule) {
+        Long different = schedule.getEnd() - schedule.getStart() ;
+
+        long secondsInMilli = 1000;
+        long minutesInMilli = secondsInMilli * 60;
+        long hoursInMilli = minutesInMilli * 60;
+        long daysInMilli = hoursInMilli * 24;
+
+        long elapsedDays = different / daysInMilli;
+        different = different % daysInMilli;
+
+        long elapsedHours = different / hoursInMilli;
+        different = different % hoursInMilli;
+
+        long elapsedMinutes = different / minutesInMilli;
+        different = different % minutesInMilli;
+
+        long elapsedSeconds = different / secondsInMilli;
+        return String.format("%d:%02d:%02d:%02d", elapsedDays, elapsedHours, elapsedMinutes, elapsedSeconds);
     }
 
     public void createOverWorkedList(Iterable<Bench> benches) {
-        XSSFSheet sheet = workbook.createSheet("Over");
+        XSSFSheet sheet = workbook.createSheet("Перегрузка");
         int counter = 0;
         for (Bench bench : benches) {
             createRow(sheet, counter++, bench.getId().toString());
@@ -93,12 +108,20 @@ public class BenchScheduleExcelFileBuilder {
     public void createSheet(Bench bench, Calendar calendar, double limit) {
         XSSFSheet sheet = workbook.createSheet(String.valueOf((bench.getId())));
         int counter = 0;
-        createRow(sheet, counter++, "Артикул", "Время начала", "Длительность");
+        createRow(sheet, counter++, "Артикул", "Колорит", "Номер заказа", "Длина", "Время начала", "Длительность", "Время окончания");
 
         List<Schedule> schedules = calendar.getSchedules();
-
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         for (Schedule schedule : schedules) {
-            createNumRow(sheet, counter++, schedule.getOperation().getSetting().toString(), schedule.getStart(), schedule.getEnd() - schedule.getStart());
+            createRow(sheet,
+                    counter++,
+                    schedule.getOperation().getOrder().getArticle().getName(),
+                    schedule.getOperation().getOrder().getArticle().getColoring(),
+                    schedule.getOperation().getOrder().getId().toString(),
+                    schedule.getOperation().getOrder().getLength().toString(),
+                    dateFormat.format(new Date(startDate.getTime() + schedule.getStart())),
+                    durationHourAndMinutes(schedule),
+                    dateFormat.format(new Date(startDate.getTime() + schedule.getEnd() - schedule.getStart())));
         }
         try {
             createChart(sheet, bench, calendar, limit);
@@ -106,6 +129,7 @@ public class BenchScheduleExcelFileBuilder {
             e.printStackTrace();
         }
     }
+
 
     //TODO IMPLEMENT IT https://github.com/knowm/XChart
     private void createChart(XSSFSheet sheet, Bench bench, Calendar calendar, double limit) throws IOException {
@@ -129,59 +153,6 @@ public class BenchScheduleExcelFileBuilder {
     private void createRealChart(XSSFSheet sheet) {
     }
 
-    private static void solidLineSeries(XDDFChartData data, int index, PresetColor color) {
-        XDDFSolidFillProperties fill = new XDDFSolidFillProperties(XDDFColor.from(color));
-        XDDFLineProperties line = new XDDFLineProperties();
-        line.setFillProperties(fill);
-        XDDFChartData.Series series = data.getSeries().get(index);
-        XDDFShapeProperties properties = series.getShapeProperties();
-        if (properties == null) {
-            properties = new XDDFShapeProperties();
-        }
-        properties.setLineProperties(line);
-        series.setShapeProperties(properties);
-    }
-
-    private static void solidFillSeries(XDDFChartData data, int index, PresetColor color) {
-        XDDFSolidFillProperties fill = new XDDFSolidFillProperties(XDDFColor.from(color));
-        XDDFChartData.Series series = data.getSeries().get(index);
-        XDDFShapeProperties properties = series.getShapeProperties();
-        if (properties == null) {
-            properties = new XDDFShapeProperties();
-        }
-        properties.setFillProperties(fill);
-        series.setShapeProperties(properties);
-    }
-
-    public static void setCatAxisTitle(XSSFChart chart, int axisIdx, String title) {
-        CTCatAx valAx = chart.getCTChart().getPlotArea().getCatAxArray(axisIdx);
-        CTTitle ctTitle = valAx.addNewTitle();
-        ctTitle.addNewLayout();
-        ctTitle.addNewOverlay().setVal(false);
-        CTTextBody rich = ctTitle.addNewTx().addNewRich();
-        rich.addNewBodyPr();
-        rich.addNewLstStyle();
-        CTTextParagraph p = rich.addNewP();
-        p.addNewPPr().addNewDefRPr();
-        p.addNewR().setT(title);
-        p.addNewEndParaRPr();
-    }
-
-
-    public static void setValueAxisTitle(XSSFChart chart, int axisIdx, String title) {
-        CTValAx valAx = chart.getCTChart().getPlotArea().getValAxArray(axisIdx);
-        CTTitle ctTitle = valAx.addNewTitle();
-        ctTitle.addNewLayout();
-        ctTitle.addNewOverlay().setVal(false);
-        CTTextBody rich = ctTitle.addNewTx().addNewRich();
-        rich.addNewBodyPr();
-        rich.addNewLstStyle();
-        CTTextParagraph p = rich.addNewP();
-        p.addNewPPr().addNewDefRPr();
-        p.addNewR().setT(title);
-        p.addNewEndParaRPr();
-    }
-
     private static void createRow(XSSFSheet sheet, int rowNum, String... cellNames) {
         Row row = sheet.createRow(rowNum);
         for (int i = 0; i < cellNames.length; i++) {
@@ -190,7 +161,7 @@ public class BenchScheduleExcelFileBuilder {
         }
     }
 
-    private static void createNumRow(XSSFSheet sheet, int rowNum, String c1, Long c2, Long c3) {
+    private static void createNumRow(XSSFSheet sheet, int rowNum, String c1, String c2, String c3) {
         Row row = sheet.createRow(rowNum);
         Cell cell0 = row.createCell(0);
         cell0.setCellValue(c1);
